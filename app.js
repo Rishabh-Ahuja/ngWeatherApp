4ec8df71ff761c1e57a69ee63c068d70be2d0e5f
Rishabh-Ahuja <ahuja.rishabh99@hotmail.com>
2017-03-08 15:26:04 +0530

    added preloading functionality

--- app.js
+++ app.js
@@ -3,48 +3,51 @@ var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
 // ROUTES
 weatherApp.config(function ($routeProvider) {
-   
+
     $routeProvider
-    
-    .when('/', {
-        templateUrl: 'pages/home.htm',
-        controller: 'homeController'
-    })
-    
-    .when('/forecast', {
-        templateUrl: 'pages/forecast.htm',
-        controller: 'forecastController'
-    })
+
+        .when('/', {
+            templateUrl: 'pages/home.htm',
+            controller: 'homeController'
+        })
+
+        .when('/forecast', {
+            templateUrl: 'pages/forecast.htm',
+            controller: 'forecastController'
+        })
         .when('/forecast/:days', {
             templateUrl: 'pages/forecast.htm',
             controller: 'forecastController'
         })
-    
+
 });
 //services
-weatherApp.service('cityService',function () {
-this.city = 'Delhi';
+weatherApp.service('cityService', function () {
+    this.city = 'Delhi';
 });
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope','cityService',function($scope,cityService) {
-   $scope.city = cityService.city;
-   $scope.$watch('city',function () {
-       cityService.city = $scope.city;
-   });
+weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService) {
+    $scope.city = cityService.city;
+    $scope.$watch('city', function () {
+        cityService.city = $scope.city;
+    });
 
 }]);
 
 
-weatherApp.controller('forecastController', ['$scope','$resource','$routeParams','cityService',function($scope,$resource,$routeParams,cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService) {
+    $scope.showCity = false;
     $scope.city = cityService.city;
     $scope.days = $routeParams.days || 2;
-    $scope.$watch('days',function () {
-        $scope.WeatherResult = $scope.WeatherApi.get({q:$scope.city,days:$scope.days})
+    $scope.$watch('days', function () {
+        $scope.WeatherResult = $scope.WeatherApi.get({q: $scope.city, days: $scope.days})
     });
-    $scope.call = 'https://api.apixu.com/v1/forecast.json?key=b8598241aafe4b0d9a424303172202';
-    $scope.WeatherApi = $resource($scope.call,{callback:'JSON_CALLBACK'});
-    $scope.WeatherResult = $scope.WeatherApi.get({q:$scope.city,days:$scope.days});
-    $scope.notFound;
-    console.log($scope.days);
-    console.log($scope.WeatherResult);
+    $scope.WeatherApi = $resource('https://api.apixu.com/v1/forecast.json?key=b8598241aafe4b0d9a424303172202', {callback: 'JSON_CALLBACK'});
+    $scope.WeatherApi.get({q: $scope.city, days: $scope.days}).$promise.then(function (response) {
+        $scope.WeatherResult = response;
+        $scope.showCity = true;
+    }, function (response) {
+        $scope.WeatherResult = response;
+        $scope.showCity = false;
+    });
 }]);
